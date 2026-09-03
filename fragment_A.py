1433a9561969c2fbb4ceeb54c376fecae5d19bc0
COD_bin-directory <testinggmail001@gmail.com>
2026-09-03 16:13:35 +0530

    remove hardcoded checker

--- fragment_A.py
+++ fragment_A.py
@@ -17,7 +17,7 @@ sha_output = hashlib.sha256((salt + candidate).encode()).hexdigest()
 print("SHA256:", sha_output)
 
 # hardcoded verification key
-target = "4034df09d41608331ada47d0bfbd03b3b2f34ec720b6e5286d6085b17e119dc9"
+target = ""
 
 if sha_output == target:
     print("MATCH")
